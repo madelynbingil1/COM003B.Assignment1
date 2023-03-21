a3a64f9bf968ab1a61e0ff0422ec61efe5dbeb75
madelynbingil <madelynbingil@LEM231-23>
2023-03-20 19:06:27 -0700

    Add project files.

---
+++ COM003B.Assignment1/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace COM003B.Assignment1.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
